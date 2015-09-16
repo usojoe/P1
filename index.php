b4b5805e128d9565f51c913d9946bf97bf76c682
Joe Lee <josephandrewlee@g.harvard.edu>
2015-09-16 15:20:25 -0400

    Modified index and css with discrtption and style code

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>Project One</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -18,7 +18,44 @@
     <![endif]-->
   </head>
   <body>
-    <h1>Hello, world!</h1>
+
+	<div class="container">
+
+<h1>Joseph Lee - Project 1</h1>
+		<img src='https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAM_AAAAJGVhZWYwODkxLTg0MjMtNGQyNC04MjVmLTA4MzU2ZWFlMDM2MA.jpg' alt='Joseph A. Lee'>
+        
+        <h3>Project 1</h3>
+        View: p1.josephandrewlee.com
+        Github: https://github.com/usojoe/P1 
+		
+		<h3>Project 2 Title</h3>
+		View: p3.josephandrewlee.com
+		Github: https://github.com/usojoe/P2
+		
+		<h3>Project 3 Title</h3>
+		View: p3.josephandrewlee.com
+		Github: https://github.com/usojoe/P3
+		
+		<h3>Project 4 Title</h3>
+		View: p4.josephandrewlee.com
+		Github: https://github.com/usojoe/P1
+		
+		<h2>In two sentences describe who you are and what you do.</h2>
+		<p>I am a multimedia journalist for the USO (United Services Organization) and my job is basically to report to the American people about the day-to-day goodness provided by their congressionally chartered nonprofit. I'm working on an ALM in Journalism at the extension school, and I should graduate next spring. Before working at the USO I was an active duty Marine Combat Correspondent for approximately 10 years.</p>
+		
+		<h2>Why are you taking this course?</h2>
+		<p>My primary reason for taking this course is to complete a certificate in web technologies, which I expect will round out my journalism experience and education moving forward into the next few decades.</p>
+		
+		<h2>What previous experience do you have with programming?</h2>
+		<p>My actual programming experience is limited to basic HTML and CSS for the most part, except I have a lot of experience with PHP and MySQL pre-packaged Content Management Systems and web forums.</p>
+		
+		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+		<p>I am working 100 percent on Mac, but I have a background in PC as well.</p>
+		
+		<h2>Anything else we should know?</h2>
+		<p>That's pretty much it. I like long walks on the beach and not pulling my hair out all night doing coding homework.</p>
+	
+	</div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
